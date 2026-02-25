a6774befe58fde9d4f35deb709432f9f6f10bdaf
Andrey Azarov <azarov.andrey@gmail.com>
2026-02-25 11:39:07 +0300

    Add cache

--- wildcard/match.go
+++ wildcard/match.go
@@ -1,6 +1,18 @@
 package wildcard
 
+type matchKey struct {
+	s string
+	p string
+}
+
+var cache = make(map[matchKey]bool)
+
 func isMatch(s string, p string) bool {
+
+	if match, ok := cache[matchKey{s: s, p: p}]; ok {
+		return match
+	}
+
 	// s is empty
 	if s == "" {
 		for _, ch := range p {
@@ -28,7 +40,9 @@ func isMatch(s string, p string) bool {
 				j++
 			}
 			if j+1 < len(p) && (p[j+1] == '?' || s[i] == p[j+1]) {
-				if isMatch(s[i:], p[j+1:]) {
+				match := isMatch(s[i:], p[j+1:])
+				cache[matchKey{s: s[i:], p: p[j+1:]}] = match
+				if match {
 					return true
 				}
 			}
